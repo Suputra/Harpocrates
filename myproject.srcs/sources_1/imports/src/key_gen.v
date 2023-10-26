8afd20de51983fbb4a9ed50dfb8fe63e7cd28cf1
Saahas Yechuri <saahas.y@gmail.com>
2023-10-26 12:43:31 -0700

    updating key gen to only change kappa

--- myproject.srcs/sources_1/imports/src/key_gen.v
+++ myproject.srcs/sources_1/imports/src/key_gen.v
@@ -23,20 +23,16 @@
 module key_gen #(lambda = 32, eta = 32, nu = 16) (
     input clk, reset,
     output finish,
-    output reg [lambda-1 : 0] p_key,
-    output reg [eta-1 : 0] q_param,
     output reg [nu-1 : 0] kappa_key
 );
     reg [7:0] acc = 10;
     reg all_prime_reset;
     reg out_reg_full = 1'b0;
 
-    wire p_done, q_done, kappa_done;
-    wire [lambda-1 : 0] p_wire;
-    wire [eta-1 : 0] q_wire;
+    wire kappa_done;
     wire [nu-1 : 0] kappa_wire;
     
-    wire [2:0] done = {p_done, q_done, kappa_done};
+    wire done = kappa_done;
     
     assign finish = (done == 3'b111) ? 1'b1 : 1'b0;
     always @(posedge clk) begin
@@ -46,28 +42,10 @@ module key_gen #(lambda = 32, eta = 32, nu = 16) (
             all_prime_reset <= 1'b0;
             out_reg_full <= 1'b0;
         end else if (finish) begin
-            p_key <= p_wire;
-            q_param <= q_wire;
             kappa_key <= kappa_wire;
 
         end
     end
-    
-    prime_gen #(.WORDSIZE(lambda)) p_gen(
-    .accuracy(acc),
-    .clk(clk),
-    .reset(all_prime_reset),
-    .done(p_done),
-    .prime_out(p_wire)
-    );
-    
-    prime_gen #(.WORDSIZE(eta)) q_gen(
-    .accuracy(acc),
-    .clk(clk),
-    .reset(all_prime_reset),
-    .done(q_done),
-    .prime_out(q_wire)
-    );
     
     prime_gen #(.WORDSIZE(nu)) kappa_gen(
     .accuracy(acc),
